a0240f4051c5d09d95bae50eca6054411350c723
Manikhandan Mavureddy Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2025-02-24 14:48:58 -0500

    updating variables for ec2 instance

--- variable.tf
+++ variable.tf
@@ -33,3 +33,25 @@ variable "pr_dest_cidr" {
   description = "destination CIDR of a public route"
   type        = string
 }
+
+variable "custom_ami_id" {
+  type        = string
+  description = "Custom AMI ID for EC2 instances"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "SSH Key pair name for EC2 instance access"
+}
+
+variable "application_port" {
+  type        = number
+  description = "Port on which the application runs"
+  default     = 8080
+}
